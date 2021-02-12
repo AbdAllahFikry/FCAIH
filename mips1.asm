c78f665e2606359ebed2ca04abf6a18bf9e222cd
AbdAllahFikry <57784380+AbdAllahFikry@users.noreply.github.com>
2021-02-13 00:09:20 +0200

    Update mips1.asm

--- mips1.asm
+++ mips1.asm
@@ -22,6 +22,25 @@ main:
     li $s1,20 #high size
     li $s2,20 #medium size
     li $s3,20 #low size   
+    
+    
+    	la	$s6, critical		# Load address of array
+	li	$s4, 'b'		# Load target value
+	
+	add 	$a0,$s6,$0 		# copy Array into argument register
+	add	$a1,$s4,$0		# copy target into argument register
+	jal	BinarySearch		# Calling BinarySearch function
+	move 	$s3, $s7 		# copy result to variable x
+	
+	bne 	$s5, -1, Else 		# result != -1
+	li      $v0, 4    		# 4 = print string
+	la      $a0,IfMessage  		# $a0 = address of the array
+	syscall		 		
+Else:
+	li      $v0,4      		# 4 = print string
+	la      $a0,ElseMessage  	# $a0 = address of the array
+	syscall	
+#--------------------------------------------------------end of test binarysearch function
     lw $a0, p1 #priority
     jal delet_one_array #call function--------------     
     lw $a0, p2 #priority
@@ -30,8 +49,7 @@ main:
     jal delet_one_array #call function--------------     
     lw $a0, p4 #priority
     jal delet_one_array #call function--------------               
-    
-    #----------------end test of calling delete 
+#-------------------------------------------------------------end test of calling delete 
     la  $a0, critical#load critical in argument a0
 	jal process    #call function process
 
@@ -46,29 +64,43 @@ main:
 	
 	li  $v0,10     #termiate the program 
 	syscall        #end of program (return 0)
-    #---------------------------end test of calling process
-	 jal empty_all_array#call function-------------- 
+    #----------------------------------------------------------------end test of calling process
+	 jal empty_all_array #call function-------------- 
      #-------------------------------end test of call empty function
 	
-	la	$s6, critical		# Load address of array
-	li	$s4, 'b'		# Load target value
-	
-	add 	$a0,$s6,$0 		# copy Array into argument register
-	add	$a1,$s4,$0		# copy target into argument register
-	addi	$a2,$a2,5		# Size of array
-	jal	BinarySearch		# Calling BinarySearch function
-	move 	$s3, $s7 		# copy result to variable x
-	
-	bne 	$s3, -1, Else 		# result != -1
-	li      $v0, 4    		# 4 = print string
-	la      $a0,IfMessage  		# $a0 = address of the array
-	syscall		 		
-Else:
-	li      $v0,4      		# 4 = print string
-	la      $a0,ElseMessage  	# $a0 = address of the array
-	syscall	
-	#-------------------------------------------end of test binarysearch function
-	
+#----------------------------------------------------------------------------------------------------------(binary search function)
+BinarySearch:
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)   		# saves $ra on stack
+	li $t1, 0 			# left = 0
+	li $t2, 0 			# middle = 0
+	subu $t3, $s2, 1 		# right = size - 1 
+	li $t4, -1 			# elementindex = -1
+whileLoop:
+	ble $t1,$t3,exit1		# left <= right
+	subu $t2, $t3, $t1 		# Mid = right - left
+	srl $t2, $t2, 1 		# Mid = (right - left) / 2
+	add $t2, $t2, $t1	 	# Mid = left + (right - left) / 2
+	sll $t6, $t2, 2    		# index for Array[Mid]
+        addu $s0, $0, $t6       	# point to mid
+	lb  $s5, 0($s6)         	# s5 = A[rraymid]	
+	bne $s5, $s4, notEqual 		# if ( Array[mid] == target)  IF not skip to notEqual	
+	add $t4, $0, $t2 		# element_index = mid
+	j exit1
+	notEqual:
+		slt $t5, $a0, $s4 	# is Array[mid] < target . IF yes, t5 = TRUE
+		beqz $t5, notLess	# go to not less if $t5 == 0
+		addi $t1, $t2, 1 	# left = mid + 1
+		j pastnotLess		# go to pastnotLess
+	notLess:	
+		subi $t3, $t2, 1 	# right = mid - 1
+	pastnotLess:
+		j whileLoop		# END WHILE
+exit1:
+	move $s7, $t4 			#return of function               
+	lw $ra, 0($sp)			#restore $ra from stack
+	addi $sp, $sp, 4		#pop $sp by 4
+	jr $ra				#return
 	
 #-----------------------------------------------------------------------------------------------(process function)
 process: #function that print requests with specific priority 
@@ -125,37 +157,4 @@ third:
 forth:
     li $s3,0  #load s3=0
     jr  $ra  #return      
-#----------------------------------------------------------------------------------------------------------(binary search function)
-BinarySearch:
-	addi $sp, $sp, -4
-	sw $ra, 0($sp)   		# saves $ra on stack
-	li $t1, 0 			# left = 0
-	li $t2, 0 			# middle = 0
-	subu $t3, $a2, 1 		# right = size - 1 
-	li $t4, -1 			# elementindex = -1
-whileLoop:
-	ble $t1,$t3,exit1		# left <= right
-	subu $t2, $t3, $t1 		# Mid = right - left
-	srl $t2, $t2, 1 		# Mid = (right - left) / 2
-	add $t2, $t2, $t1	 	# Mid = left + (right - left) / 2
-	sll $t6, $t2, 2    		# index for Array[Mid]
-        addu $s0, $0, $t6       	# point to mid
-	lb  $s5, 0($s6)         	# s5 = A[rraymid]	
-	bne $s5, $s4, notEqual 		# if ( Array[mid] == target)  IF not skip to notEqual	
-	add $t4, $0, $t2 		# element_index = mid
-	j exit1
-	notEqual:
-		slt $t5, $a0, $s4 	# is Array[mid] < target . IF yes, t5 = TRUE
-		beqz $t5, notLess	# go to not less if $t5 == 0
-		addi $t1, $t2, 1 	# left = mid + 1
-		j pastnotLess		# go to pastnotLess
-	notLess:	
-		subi $t3, $t2, 1 	# right = mid - 1
-	pastnotLess:
-		j whileLoop		# END WHILE
-exit1:
-	move $s7, $t4 			#return of function               
-	lw $ra, 0($sp)			#restore $ra from stack
-	addi $sp, $sp, 4		#pop $sp by 4
-	jr $ra				#return
 #------------------------------------------------------------------------------------------------------------------------------------
